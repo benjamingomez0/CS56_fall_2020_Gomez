30a2b3f73664f0cc8e2c56ca3ab79276aa2a21f8
benjamingomez0 <benjamingomez0@gmail.com>
2020-09-12 19:27:38 -0700

    add Item.java class. add package assignment_01

---
+++ assignment_01/Item.java
@@ -0,0 +1,42 @@
+package assignment_01;
+public class Item {
+    // 
+    private String name;
+    private String description;
+
+    //constructors
+    Item ()
+    {
+        this.name = "";
+        this.description = "";
+    }
+    Item (String name, String description)
+    {
+        this.name = name;
+        this.description = description;
+    }
+
+    // Accessor functions
+
+    public String getName ()
+    {
+        return this.name;
+    }
+
+    public String getDescription ()
+    {
+        return this.description;
+    }
+
+    // Mutator functions
+
+    public void setName (String name)
+    {
+        this.name = name;
+    }
+
+    public void setDescription (String description)
+    {
+        this.description = description;
+    }
+}
---
+++ assignment_01/main.java
@@ -0,0 +1,17 @@
+package assignment_01;
+
+public  class main{    
+    public static void main (String [] args)
+    {
+        Item item_1 = new Item("Item 1", "The First Item");
+        System.out.println("Item 1 name: " + item_1.getName());
+        System.out.println("Item 1 description: " + item_1.getDescription());
+        item_1.setName("box");
+        item_1.setDescription("This is a box");
+        System.out.println("New Item 1 name: " + item_1.getName());
+        System.out.println("New Item 1 description: " + item_1.getDescription());
+    }
+
+
+
+}
